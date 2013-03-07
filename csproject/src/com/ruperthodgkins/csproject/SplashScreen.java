0951c0feaf632e8dd6751c1cb26f8365b49e1350
Rupert Hodgkins <hodgkinsr@hotmail.co.uk>
2013-03-07 22:36:34 +0000

    Added SplashScreen
    Minor change

--- csproject/src/com/ruperthodgkins/csproject/SplashScreen.java
+++ csproject/src/com/ruperthodgkins/csproject/SplashScreen.java
@@ -39,7 +39,7 @@ public class SplashScreen implements Screen {
 	
 	@Override
 	public void render(float delta) {
-		System.out.println(alpha);
+		//System.out.println(alpha);
 		if(fadedOut) main.setScreen(new LoadingScreen(main));
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
